06b77ccd424790725bfe39609d650f991d4b4a0e
Craig Norris <craignorris87@me.com>
2024-12-28 16:59:09 +0000

    Updates

--- Hangman/Form1.Designer.cs
+++ Hangman/Form1.Designer.cs
@@ -39,9 +39,31 @@
             panel1 = new Panel();
             label3 = new Label();
             panel2 = new Panel();
+            hangmanPic7c = new PictureBox();
+            hangmanPic7b = new PictureBox();
+            hangmanPic7a = new PictureBox();
+            hangmanPic6b = new PictureBox();
+            hangmanPic6a = new PictureBox();
+            hangmanPic5 = new PictureBox();
+            hangmanPic4 = new PictureBox();
+            hangmanPic3 = new PictureBox();
+            hangmanPic2 = new PictureBox();
+            hangmanPic1 = new PictureBox();
             triesLabel = new Label();
+            label4 = new Label();
+            guessedLettersLabel = new Label();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic7c).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic7b).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic7a).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic6b).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic6a).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic1).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -140,6 +162,18 @@
             // 
             // panel2
             // 
+            panel2.Controls.Add(guessedLettersLabel);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(hangmanPic7c);
+            panel2.Controls.Add(hangmanPic7b);
+            panel2.Controls.Add(hangmanPic7a);
+            panel2.Controls.Add(hangmanPic6b);
+            panel2.Controls.Add(hangmanPic6a);
+            panel2.Controls.Add(hangmanPic5);
+            panel2.Controls.Add(hangmanPic4);
+            panel2.Controls.Add(hangmanPic3);
+            panel2.Controls.Add(hangmanPic2);
+            panel2.Controls.Add(hangmanPic1);
             panel2.Controls.Add(triesLabel);
             panel2.Controls.Add(label5);
             panel2.Controls.Add(wordToFindLabel);
@@ -148,8 +182,108 @@
             panel2.Controls.Add(guessTextBox);
             panel2.Location = new Point(12, 121);
             panel2.Name = "panel2";
-            panel2.Size = new Size(462, 204);
+            panel2.Size = new Size(552, 285);
             panel2.TabIndex = 10;
+            // 
+            // hangmanPic7c
+            // 
+            hangmanPic7c.BackColor = SystemColors.ActiveCaptionText;
+            hangmanPic7c.Location = new Point(501, 155);
+            hangmanPic7c.Name = "hangmanPic7c";
+            hangmanPic7c.Size = new Size(10, 30);
+            hangmanPic7c.TabIndex = 18;
+            hangmanPic7c.TabStop = false;
+            hangmanPic7c.Visible = false;
+            // 
+            // hangmanPic7b
+            // 
+            hangmanPic7b.BackColor = SystemColors.ActiveCaptionText;
+            hangmanPic7b.Location = new Point(475, 155);
+            hangmanPic7b.Name = "hangmanPic7b";
+            hangmanPic7b.Size = new Size(10, 30);
+            hangmanPic7b.TabIndex = 13;
+            hangmanPic7b.TabStop = false;
+            hangmanPic7b.Visible = false;
+            // 
+            // hangmanPic7a
+            // 
+            hangmanPic7a.BackColor = SystemColors.ActiveCaptionText;
+            hangmanPic7a.Location = new Point(475, 141);
+            hangmanPic7a.Name = "hangmanPic7a";
+            hangmanPic7a.Size = new Size(36, 14);
+            hangmanPic7a.TabIndex = 17;
+            hangmanPic7a.TabStop = false;
+            hangmanPic7a.Visible = false;
+            // 
+            // hangmanPic6b
+            // 
+            hangmanPic6b.BackColor = SystemColors.ActiveCaptionText;
+            hangmanPic6b.Location = new Point(497, 91);
+            hangmanPic6b.Name = "hangmanPic6b";
+            hangmanPic6b.Size = new Size(36, 14);
+            hangmanPic6b.TabIndex = 16;
+            hangmanPic6b.TabStop = false;
+            hangmanPic6b.Visible = false;
+            // 
+            // hangmanPic6a
+            // 
+            hangmanPic6a.BackColor = SystemColors.ActiveCaptionText;
+            hangmanPic6a.Location = new Point(453, 91);
+            hangmanPic6a.Name = "hangmanPic6a";
+            hangmanPic6a.Size = new Size(36, 14);
+            hangmanPic6a.TabIndex = 12;
+            hangmanPic6a.TabStop = false;
+            hangmanPic6a.Visible = false;
+            // 
+            // hangmanPic5
+            // 
+            hangmanPic5.BackColor = SystemColors.ActiveCaptionText;
+            hangmanPic5.Location = new Point(488, 75);
+            hangmanPic5.Name = "hangmanPic5";
+            hangmanPic5.Size = new Size(10, 66);
+            hangmanPic5.TabIndex = 12;
+            hangmanPic5.TabStop = false;
+            hangmanPic5.Visible = false;
+            // 
+            // hangmanPic4
+            // 
+            hangmanPic4.BackColor = SystemColors.ActiveCaptionText;
+            hangmanPic4.Location = new Point(478, 44);
+            hangmanPic4.Name = "hangmanPic4";
+            hangmanPic4.Size = new Size(30, 31);
+            hangmanPic4.TabIndex = 14;
+            hangmanPic4.TabStop = false;
+            hangmanPic4.Visible = false;
+            // 
+            // hangmanPic3
+            // 
+            hangmanPic3.BackColor = SystemColors.ActiveCaptionText;
+            hangmanPic3.Location = new Point(488, 19);
+            hangmanPic3.Name = "hangmanPic3";
+            hangmanPic3.Size = new Size(10, 25);
+            hangmanPic3.TabIndex = 13;
+            hangmanPic3.TabStop = false;
+            hangmanPic3.Visible = false;
+            // 
+            // hangmanPic2
+            // 
+            hangmanPic2.BackColor = SystemColors.ActiveCaptionText;
+            hangmanPic2.Location = new Point(382, 9);
+            hangmanPic2.Name = "hangmanPic2";
+            hangmanPic2.Size = new Size(116, 10);
+            hangmanPic2.TabIndex = 12;
+            hangmanPic2.TabStop = false;
+            hangmanPic2.Visible = false;
+            // 
+            // hangmanPic1
+            // 
+            hangmanPic1.BackColor = SystemColors.ActiveCaptionText;
+            hangmanPic1.Location = new Point(382, 19);
+            hangmanPic1.Name = "hangmanPic1";
+            hangmanPic1.Size = new Size(10, 180);
+            hangmanPic1.TabIndex = 11;
+            hangmanPic1.TabStop = false;
+            hangmanPic1.Visible = false;
             // 
             // triesLabel
             // 
@@ -161,11 +295,30 @@
             triesLabel.TabIndex = 10;
             triesLabel.Text = "7";
             // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(7, 204);
+            label4.Name = "label4";
+            label4.Size = new Size(159, 30);
+            label4.TabIndex = 19;
+            label4.Text = "Letters Guessed";
+            // 
+            // guessedLettersLabel
+            // 
+            guessedLettersLabel.AutoSize = true;
+            guessedLettersLabel.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            guessedLettersLabel.Location = new Point(176, 204);
+            guessedLettersLabel.Name = "guessedLettersLabel";
+            guessedLettersLabel.Size = new Size(0, 30);
+            guessedLettersLabel.TabIndex = 20;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(481, 334);
+            ClientSize = new Size(580, 418);
             Controls.Add(panel2);
             Controls.Add(panel1);
             Controls.Add(label1);
@@ -175,6 +328,16 @@
             panel1.PerformLayout();
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic7c).EndInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic7b).EndInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic7a).EndInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic6b).EndInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic6a).EndInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -193,5 +356,17 @@
         private Label label3;
         private Panel panel2;
         private Label triesLabel;
+        private PictureBox hangmanPic7a;
+        private PictureBox hangmanPic6b;
+        private PictureBox hangmanPic6a;
+        private PictureBox hangmanPic5;
+        private PictureBox hangmanPic4;
+        private PictureBox hangmanPic3;
+        private PictureBox hangmanPic2;
+        private PictureBox hangmanPic1;
+        private PictureBox hangmanPic7b;
+        private PictureBox hangmanPic7c;
+        private Label guessedLettersLabel;
+        private Label label4;
     }
 }
--- Hangman/Form1.cs
+++ Hangman/Form1.cs
@@ -5,6 +5,7 @@ namespace Hangman
         string wordStored = string.Empty;
         List<char> letters = new List<char>();
         int numberOfTries = 0;
+        bool finished = false;
 
         public Form1()
         {
@@ -13,7 +14,12 @@ namespace Hangman
 
         private void wordStoreButton_Click(object sender, EventArgs e)
         {
-            wordStored = wordTextBox.Text; //Here we are storing the text in the textbox into our string variable which we named wordStored
+            ResetHangman();
+
+            finished = false;
+            guessedLettersLabel.Text = string.Empty;
+            wordStored = wordTextBox.Text.ToUpper(); //Here we are storing the text in the textbox into our string variable which we named wordStored
+            wordTextBox.Text = string.Empty;
             wordToFindLabel.Text = new string('_', wordStored.Length); //Here we are setting the label so a player can see how many letters to guess.
                                                                        // we replace the letters with "_" so the word is hidden
             letters = new List<char>();
@@ -25,7 +31,14 @@ namespace Hangman
 
         private void guessButton_Click(object sender, EventArgs e)
         { 
-            char guessLetter = guessTextBox.Text[0]; 
+            if(finished)
+            {
+                MessageBox.Show("Game has completed, start a new game");
+
+                return;
+            }
+
+            char guessLetter = guessTextBox.Text.ToUpper()[0]; 
 
             foreach (char letter in letters) 
             { 
@@ -39,33 +52,96 @@ namespace Hangman
 
             letters.Add(guessLetter);
 
-            if(!wordStored.Contains(guessLetter))
+            if (!wordStored.Contains(guessLetter) && numberOfTries > 0)
             {
+                guessedLettersLabel.Text = string.Join(" ", guessedLettersLabel.Text, guessLetter);
                 numberOfTries--;
 
                 triesLabel.Text = numberOfTries.ToString();
+
+                SetHangman();
             }
 
             if (numberOfTries == 0)
             {
+                finished = true;
                 MessageBox.Show("No more guesses, the word was " + wordStored);
+
+                return;
             }
 
             string displayWord = string.Empty;
+
+            int letterFound = 0;
+            int storedWordCount = wordStored.Length;
 
             foreach (char letter in wordStored) 
             {
                 if(letters.Contains(letter))
                 {
-                    displayWord = string.Concat(displayWord, " " + letter + " ");
+                    displayWord = string.Concat(displayWord, letter);
+
+                    letterFound++;
                 }
                 else
                 {
-                    displayWord = string.Concat(displayWord, " _ ");
+                    displayWord = string.Concat(displayWord, "_");
                 }
             }
 
             wordToFindLabel.Text = displayWord;
+
+            if (letterFound == storedWordCount)
+            {
+                finished = true;
+
+                MessageBox.Show("You win!");
+            }
+        }
+
+        private void ResetHangman()
+        {
+            hangmanPic1.Visible = false;
+            hangmanPic2.Visible = false;
+            hangmanPic3.Visible = false;
+            hangmanPic4.Visible = false;
+            hangmanPic5.Visible = false;
+            hangmanPic6a.Visible = false;
+            hangmanPic6b.Visible = false;
+            hangmanPic7a.Visible = false;
+            hangmanPic7b.Visible = false;
+            hangmanPic7c.Visible = false;
+        }
+
+        private void SetHangman()
+        {
+            switch(numberOfTries)
+            {
+                case 6:
+                    hangmanPic1.Visible = true;
+                    break;
+                case 5:
+                    hangmanPic2.Visible = true;
+                    break;
+                case 4:
+                    hangmanPic3.Visible = true;
+                    break;
+                case 3:
+                    hangmanPic4.Visible = true;
+                    break;
+                case 2:
+                    hangmanPic5.Visible = true;
+                    break;
+                case 1:
+                    hangmanPic6a.Visible = true;
+                    hangmanPic6b.Visible = true;
+                    break;
+                case 0:
+                    hangmanPic7a.Visible = true;
+                    hangmanPic7b.Visible = true;
+                    hangmanPic7c.Visible = true;
+                    break;
+            }
         }
     }
 }
